91e6e41424e48228349a7c1dceb957cf9abe9142
Eric Mai <eric.mai@ekami.ch>
2023-10-04 03:48:49 +0200

    Movable contact card

--- script.js
+++ script.js
@@ -44,7 +44,6 @@ function changeSection(direction) {
         document.querySelector("header > nav > article:nth-child(" + (activeSection - 1) + ")").classList.add("active");
         activeSection--;
     }
-
     document.querySelector("header nav").scrollLeft += document.querySelector("header .active").getBoundingClientRect().left - 10;
 }
 
@@ -56,7 +55,7 @@ logo = document.querySelector("#main-logo");
 
 
 
-document.addEventListener("touchmove", () => {
+document.querySelector("main").addEventListener("touchmove", () => {
     var st = document.querySelector("main").scrollTop;
     if (st > lastScrollTop) {
         // downscroll
@@ -120,7 +119,7 @@ document.addEventListener("scroll", () => {
         if (activeSection != 1 && activeSection != document.querySelectorAll("main section").length && (element.getBoundingClientRect().top * -1) / (element.offsetHeight - (window.innerHeight / 2)) * 85 > 0 && (element.getBoundingClientRect().top * -1) / (element.offsetHeight - (window.innerHeight / 2)) * 85 < 85) {
             document.querySelector("header > nav > article.active h2").style.transform = "translateX(" + ((element.getBoundingClientRect().top * -1) / (element.offsetHeight - (window.innerHeight / 2)) * 85) * -1 + "%)";
         }
-        else if (activeSection == document.querySelectorAll("main section").length && ((element.getBoundingClientRect().top * -1) / (element.offsetHeight - window.innerHeight) * 85) > 0 ) {
+        else if (activeSection == document.querySelectorAll("main section").length && ((element.getBoundingClientRect().top * -1) / (element.offsetHeight - window.innerHeight) * 85) > 0) {
             document.querySelector("header > nav > article.active h2").style.transform = "translateX(" + ((element.getBoundingClientRect().top * -1) / (element.offsetHeight - window.innerHeight) * 85) * -1 + "%)";
         }
         else if (Array.prototype.indexOf.call(document.querySelectorAll("main section"), element) == activeSection - 1 && 100 - ((element.getBoundingClientRect().top * -1) - element.offsetHeight + window.innerHeight / 2) / -element.offsetHeight * 100 > 0 && activeSection != 1) {
@@ -138,6 +137,68 @@ document.querySelectorAll("header > nav > article").forEach(element => {
 })
 
 
+function showContactCard() {
+    if (document.querySelector("#contact-card").classList.contains("active")) {
+        document.querySelector("#contact-card").classList.remove("active");
+        document.querySelector("#contact-card-button .icon").innerHTML = '<svg width="40" height="40" version="1.1" id="Calque_4" xmlns="http://www.w3.org/2000/svg"                     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 800 800"                     style="enable-background:new 0 0 800 800;" xml:space="preserve">                     <path d="M133.3,642.7c0,50,40.6,90.6,90.6,90.6h352.2c50,0,90.6-40.6,90.6-90.6V157.3c0-50-40.6-90.6-90.6-90.6H223.9            c-50,0-90.6,40.6-90.6,90.6V642.7z M400,250c36.8,0,66.7,29.8,66.7,66.7s-29.8,66.7-66.7,66.7s-66.7-29.8-66.7-66.7            S363.2,250,400,250z M273.7,540.5c23.6-69.8,99.3-107.2,169-83.6c39.4,13.3,70.3,44.2,83.6,83.6c5.4,17.5-4.4,36-21.9,41.4            c-3.3,1-6.7,1.5-10.1,1.5H306c-18.4,0.2-33.5-14.6-33.7-33C272.3,547,272.7,543.7,273.7,540.5z" />                 </svg>'
+    }
+    else {
+        document.querySelector("#contact-card").classList.add("active");
+        document.querySelector("#contact-card-button .icon").innerHTML = '<svg id="Calque_5" width="40" height="40" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"                 onclick="showContactCard()">                 <defs>                     <style>                         .cls-1-contact {                             fill: none;                             stroke: #04183B;                             stroke-linecap: round;                             stroke-width: 56px;                         }                          .cls-1-contact,                         .cls-2-contact {                             stroke-miterlimit: 10;                         }                     </style>                 </defs>                 <circle class="cls-2-contact" cx="500" cy="500" r="400" />                 <g>                     <line class="cls-1-contact" x1="302.01" y1="302.01" x2="697.99" y2="697.99" />                     <line class="cls-1-contact" x1="302.01" y1="697.99" x2="697.99" y2="302.01" />                 </g>             </svg>'
+    }
+}
+
+var contactCard = document.querySelector("#contact-card");
+var contactDragArea = document.querySelector("#window-header");
+var isDraggingContact = false;
+var dragOffsetContact = { x: 0, y: 0 };
+
+contactDragArea.addEventListener("mousedown", function (event) {
+    isDraggingContact = true;
+
+    // Calculate the offset between the mouse position and the top-left corner of the div
+    dragOffsetContact.x = event.clientX - contactCard.offsetLeft;
+    dragOffsetContact.y = event.clientY - contactCard.offsetTop;
+});
+
+document.addEventListener("mousemove", function (event) {
+    if (isDraggingContact) {
+        // Calculate the new position of the div
+        var newLeft = event.clientX - dragOffsetContact.x;
+        var newTop = event.clientY - dragOffsetContact.y;
+
+        // Get the boundaries of the parent element
+        var parentRect = document.querySelector("main").getBoundingClientRect();
+
+        // Check if the new position is outside the parent element
+        if (newLeft < parentRect.left) {
+            newLeft = parentRect.left;
+        }
+        else if (newLeft + contactCard.offsetWidth > parentRect.right) {
+            newLeft = parentRect.right - contactCard.offsetWidth;
+        }
+
+        if (newTop < 0) {
+            newTop = 0;
+        }
+        else if (newTop + contactDragArea.offsetHeight > window.innerHeight) {
+            newTop = window.innerHeight - contactDragArea.offsetHeight;
+        }
+
+        // Move the div to the new position
+        contactCard.style.left = newLeft + "px";
+        contactCard.style.top = newTop + "px";
+
+        document.querySelector("body").style.userSelect = "none";
+    }
+    else {
+        document.querySelector("body").style.userSelect = "auto";
+    }
+});
+
+document.addEventListener("mouseup", function (event) {
+    isDraggingContact = false;
+});
 
 
 
